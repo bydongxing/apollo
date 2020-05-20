4c3953e2a351a37ca5d86ebe9c981618d3b56df4
Dongxing <bydongxing@163.com>
2020-05-20 09:43:31 +0800

    CLuster CRUD

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/ClusterController.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/ClusterController.java
@@ -6,17 +6,9 @@ import com.ctrip.framework.apollo.portal.service.ClusterService;
 import com.ctrip.framework.apollo.portal.spi.UserInfoHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Objects;
-
-import static com.ctrip.framework.apollo.common.utils.RequestPrecondition.checkModel;
 
 @RestController
 public class ClusterController {
@@ -33,10 +25,11 @@ public class ClusterController {
   @PostMapping(value = "apps/{appId}/envs/{env}/clusters")
   public ClusterDTO createCluster(@PathVariable String appId, @PathVariable String env,
                                   @Valid @RequestBody ClusterDTO cluster) {
+    // 设置 ClusterDTO 的创建和修改人为当前管理员
     String operator = userInfoHolder.getUser().getUserId();
     cluster.setDataChangeLastModifiedBy(operator);
     cluster.setDataChangeCreatedBy(operator);
-
+    // 创建 Cluster 到 Admin Service
     return clusterService.createCluster(Env.valueOf(env), cluster);
   }
 
